4c48222834fd9d0de93768c314512c51d16dd0dc
Jordan <JMCollins0@aol.com>
2023-04-10 11:06:51 -0400

    Button fetches random advice via advice id

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import AdviceCard from './Components/adviceCard';
+import { useEffect, useState } from "react";
+import "./App.css";
+import AdviceCard from "./Components/adviceCard";
+import diceIcon from"../src/Assets/images/icon-dice.svg";
 
 function App() {
-  // The fetch to the API is to be done here as opposed to the components
-  let [adviceData,assignData] = useState([]);
-  useEffect(()=>{ // This will trigger a warning but each render is meant to be different
-    async function fetchData(){
-    await fetch("https://api.adviceslip.com/advice")
-    .then(res=>{
-      return res.json()
-    })
-    .then(res =>{
-      assignData(res.slip)
-    })
+  // Fetch is done here because display component is for display only
+  let [adviceData, assignData] = useState([]);
+  function getData() {
+    let slip_id = Math.round(Math.random() * 224)
+    console.log(slip_id)
+    function fetchData() {
+      fetch(`https://api.adviceslip.com/advice/${slip_id}`)
+        .then((res) => {
+          return res.json();
+        })
+        .then((res) => {
+          console.log(res);
+          assignData(res.slip);
+        });
+    }
+    fetchData();
   }
-  fetchData()
-  },[])
+  useEffect(()=>{
+    getData()
+  }, []);
+
   return (
     <div className="App">
-      <AdviceCard adviceID={adviceData.id} adviceText={adviceData.advice}/>
+      <AdviceCard adviceID={adviceData.id} adviceText={adviceData.advice} />
+      <button onClick={getData}><img src={diceIcon} alt="Dice"></img></button>
     </div>
   );
 }
